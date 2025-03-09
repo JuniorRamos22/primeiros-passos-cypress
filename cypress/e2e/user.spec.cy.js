8e81a4a1dfdd4555cb828798749d163264fee7f4
unknown <reginaldoramos05@gmail.com>
2025-03-08 21:15:00 -0300

    User update scenario added

---
+++ cypress/e2e/user.spec.cy.js
@@ -0,0 +1,56 @@
+describe('Orage HRM tests', () => {
+
+  const selectorList = {
+    usernameField: "[name='username']",
+    passwordField: "[name='password']",
+    loginButton: "[type='submit']",
+    sectionTitleTopBar: ".oxd-topbar-hearder-breadcrumb-module",
+    dashboardGrid: ".orangehrm-dashboard-grid",
+    wrongCredentialAlert: "[role='alert']",
+    myInforbutton: '[href="/web/index.php/pim/viewMyDetails"]',
+    firstNameField: "[name='firstName']",
+    lastNameField: "[name='lastName']",
+    genericField: ".oxd-input--active",
+    dateCloseButton: ".--close",
+    submitButoon: "[type='submit']"
+  }
+
+  const userData = {
+    userSuccess: {
+      username: 'Admin',
+      password: 'admin123'
+    },
+    userFail: {
+      username: 'teste',
+      password: 'teste'
+    }
+  }
+
+  it.only('User Info Update - Success', () => {
+    cy.visit('/auth/login')
+    cy.get(selectorList.usernameField).type(userData.userSuccess.username)
+    cy.get(selectorList.passwordField).type(userData.userSuccess.password)
+    cy.get(selectorList.loginButton).click()
+    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+    cy.get(selectorList.dashboardGrid)
+    cy.get(selectorList.myInforbutton).click()
+    cy.get(selectorList.firstNameField).clear().type('FirstNameTest')
+    cy.get(selectorList.lastNameField).clear().type('lestNameTest')
+    cy.get(selectorList.genericField).eq(2).clear().type('NickTest')
+    cy.get(selectorList.genericField).eq(3).clear().type('Employee')
+    cy.get(selectorList.genericField).eq(4).clear().type('OtherIdTest')
+    cy.get(selectorList.genericField).eq(5).clear().type('DriversLicenseTest')
+    cy.get(selectorList.genericField).eq(6).clear().type('2025-03-10')
+    cy.get(selectorList.dateCloseButton).click()
+    cy.get(selectorList.genericField).eq(8).clear().type('sinNuberTest')
+    cy.get(selectorList.submitButoon).eq(0).click()
+    cy.get('body').should('contain', 'Successfully Updated')
+  })
+  it('Login - Fail', () => {
+    cy.visit('/auth/login')
+    cy.get(selectorList.usernameField).type(userData.userFail.username)
+    cy.get(selectorList.passwordField).type(userData.userFail.password)
+    cy.get(selectorList.loginButton).click()
+    cy.get(selectorList.wrongCredentialAlert)
+  })
+})
